e375dc85075f8d9ea78d28f4357813b17f69e854
frhd <farhadino@gmail.com>
2023-09-10 21:59:33 +0200

    testing QT

--- main.cpp
+++ main.cpp
@@ -1,20 +1,19 @@
-#include <iostream>
-// use math library
-#include <cmath>
+#include <QApplication>
+#include <QPushButton>
 
-// a function to calculate the square root of a number
-double square_root(const double a) {
-    // if a is negative, throw an exception
-    if (a < 0.0) {
-        throw "Can not take square root of negative number";
-    }
-    // otherwise, return the square root of a
-    return sqrt(a);
-}
+int main(int argc, char *argv[]) {
+    QApplication a(argc, argv);
+    QPushButton button("Hello world!", nullptr);
+    button.resize(200, 100);
+    button.show();
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    // call square_root function
-    std::cout << square_root(2.0) << std::endl;
-    return 0;
+    // if button clicked, then add a counter
+    int counter = 0;
+    QObject::connect(&button, &QPushButton::clicked, [&button, &counter]() {
+        counter++;
+        button.setText(QString("Clicked %1 times").arg(counter));
+        qDebug() << "Button clicked" << counter << "times";
+    });
+
+    return QApplication::exec();
 }
