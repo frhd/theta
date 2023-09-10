eb06e89eeff536c86fb0ba1dc07f933c235db1a2
frhd <farhadino@gmail.com>
2023-09-10 22:37:26 +0200

    testing buttons

--- main.cpp
+++ main.cpp
@@ -3,17 +3,23 @@
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
-    QPushButton button("Hello world!", nullptr);
+
+    // put the button into a big window
+    QWidget window;
+    window.resize(400, 400);
+    window.show();
+    // add a button into the window
+    QPushButton button("Hello world!", &window);
     button.resize(200, 100);
     button.show();
-
     // if button clicked, then add a counter
-    int counter = 0;
+    int counter= 0;
     QObject::connect(&button, &QPushButton::clicked, [&button, &counter]() {
         counter++;
         button.setText(QString("Clicked %1 times").arg(counter));
-        qDebug() << "Button clicked" << counter << "times";
+        qDebug() << "Button clicked" << counter<< "times";
     });
+
 
     return QApplication::exec();
 }
